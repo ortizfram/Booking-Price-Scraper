77c08114764d5930fc52b6a48744d1c197015623
melon <ortizfranco48@gmail.com>
2023-02-18 11:20:14 -0300

    changing with open 'w' to overwrite

--- script.py
+++ script.py
@@ -56,7 +56,7 @@ for name in hotel_names:
             continue
 
     # Write the search results to a file
-    with open(f'{name}_search_results.txt', 'x') as f:
+    with open(f'{name}_search_results.txt', 'w') as f:
         for result in search_results:
             f.write(f'Name: {result[0]} | Occupancy: {result[1]} | Price: {result[2]}\n')
 
