bf44cd2ab6e7756bc5dbee9a879f9288d66c6e71
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2023-02-21 11:35:24 -0300

    Select Property block data bs4

--- script.py
+++ script.py
@@ -19,29 +19,16 @@ all_results = []
 for occupancy in [2, 3]:
 
         # Construct the URL for the hotel page on Booking.com
-        url = 'https://www.booking.com/searchresults.en-gb.html?ss={location}&ssne={location}&ssne_untouched={location}&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_type=city&checkin={checkin_date}&checkout={checkout_date}&group_adults={occupancy}&no_rooms=1&group_children=0&sb_travel_purpose=leisure'    
+        url = f"https://www.booking.com/searchresults.en-gb.html?ss={location}&ssne={location}&ssne_untouched={location}&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_type=city&checkin={checkin_date}&checkout={checkout_date}&group_adults={occupancy}&no_rooms=1&group_children=0&sb_travel_purpose=leisure"    
         # define headers
         response = requests.get(url)
         headers = response.headers
         response = requests.get(url, headers=headers)
 
-        soup = BeautifulSoup(response.content,'lxml')
+        soup = BeautifulSoup(response.content,'html5lib')
 
         # Select Property block
         for item in soup.select('.property-card'):
-                print(item)
+                title = item.select('[data-testid="title"]')[0].get_text().strip()
+                print(title)
                 print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-#---------------------------------------------
-# SAVE TO A FILE
-
-# # Write all search results to a single file
-# with open('search_results.txt', 'w') as f:
-#     for result in all_results:
-#         f.write(f'Hotel: {result[0]} | Occupancy: {result[1]} | Name: {result[2]} | Price: {result[3]}\n')
-
-# # Open the file to show the search results
-# with open('search_results.txt', 'r') as f:
-#     print(f.read())
-
-# # Quit the web driver
-# driver.quit()
