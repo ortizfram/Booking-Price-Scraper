b31b6ec250b9ec1178573098c362a12df05268a4
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2023-02-21 12:41:01 -0300

    Initialize html parser page2

--- script.py
+++ script.py
@@ -1,34 +1,15 @@
-import pandas as pd
-from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import requests
-# pip install lxml 
 
-# Define the list of hotel names to search for
-names_path = pd.read_csv('competitors_test.csv')
-hotel_names = names_path['competitors_names']
-location = names_path['location']
-checkin_date =   pd.Timestamp.now().strftime('%Y-%m-%d')
-checkout_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+'''go to booking.com, put desired location,occupancy quantity, and dates. Then press enter and 
+paste those lnks here '''
 
-# Create a list to store all search results
-all_results = []
+url_2pax = "https://www.booking.com/searchresults.en-gb.html?ss=Mendoza&ssne=Mendoza&ssne_untouched=Mendoza&label=gen173nr-1FCAEoggI46AdIM1gEaAyIAQGYAQm4ARfIAQzYAQHoAQH4AQuIAgGoAgO4Au3_0p8GwAIB0gIkNDA3M2Q2ZjMtZjZjOC00ZWNjLWFmNjQtZWFlZDlmYjVjMTc22AIG4AIB&sid=d8b713e5b1e956005a30a6976b1dc703&aid=304142&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=-1003869&dest_type=city&checkin=2023-02-21&checkout=2023-02-22&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure"
+page2= requests.get(url_2pax)
+# url3=
+# url4=
+# url5=
+# url6=
 
-# Loop through each hotel name and extract its name and price for different occupancies
-
-for occupancy in [2, 3]:
-
-        # Construct the URL for the hotel page on Booking.com
-        url = f"https://www.booking.com/searchresults.en-gb.html?ss={location}&ssne={location}&ssne_untouched={location}&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_type=city&checkin={checkin_date}&checkout={checkout_date}&group_adults={occupancy}&no_rooms=1&group_children=0&sb_travel_purpose=leisure"    
-        # define headers
-        response = requests.get(url)
-        headers = response.headers
-        response = requests.get(url, headers=headers)
-
-        soup = BeautifulSoup(response.content,'html5lib')
-
-        # Select Property block
-        for item in soup.select('.property-card'):
-                title = item.select('[data-testid="title"]')[0].get_text().strip()
-                print(title)
-                print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+soup = BeautifulSoup(page2.content, 'html.parser')
+print(soup)
